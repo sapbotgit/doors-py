39301f2729948cffe724c684f9255f9e276bc9d7
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2022-12-10 12:55:24 +0500

    Library

---
+++ doors.py
@@ -0,0 +1,25 @@
+from art import tprint
+import time
+def door():
+    print("Это не доработано")
+def doors():
+    door = 0
+    print("Добро пожаловать в DOORS...")
+    time.sleep(3)
+    door()
+
+def over(monster):
+    tprint("YOU DIED!")
+    time.sleep(3)
+    print("Вас убил " + monster)
+    time.sleep(5)
+    if monster == "Rush":
+        print("Если пришёл Rush попробуйте спрятатся в шкаф")
+
+    if monster == "Exiter":
+        print("Если вы не хотите быть убитым Exiter'ом:")
+        print("Не выходите из игры пока не зайдёте в игру")
+
+    if monster == "Non-wanter":
+        print("Non-wanter убивает когда долгий выбор")
+        
