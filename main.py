63e398b885fec8cbb55e16914d236a9de95547fb
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2022-12-10 12:54:39 +0500

    Main of the code

---
+++ main.py
@@ -0,0 +1,16 @@
+import doors as d
+from art import tprint
+
+tprint("DOORS")
+print("Портировано SCS под питон (Сразу на русском")
+print("(Не-графическая версия)")
+a = input("Play Y\N? ")
+if a == "Y":
+    d.doors()
+
+else:
+    print("До следуещего запуска!")
+    d.over("Exiter")
+
+tprint("END of scripts")
+time.sleep(5)
